af92e8db84ee505b41677218b0b5a01aaa8b7d57
Harry <harry@firedrop.ai>
2020-04-14 20:24:49 +0100

    Removed old files and new redirects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import draw from "./dots";
 import Header from "./Header";
 import Logs from "./Logs";
 import About from "./About";
-import Sawce from "./Sawce";
+
+const CV_URL =
+  "https://docs.google.com/document/d/1-5dOXJVj-76sywsWTxtBaWhnCBNOGsGiiUu0jFondoA/edit?usp=sharing";
+
+const BOX_INSTALL_SCRIPT =
+  "https://gist.githubusercontent.com/mogwai/1821b3950c2d4e760c5db72ca160b408/raw/93491f82b3ea032e759a3abb3967a54602e037a6/gistfile1.txt";
+function relocate(location) {
+  return () => (window.location.href = location);
+}
 
 class App extends React.Component {
   componentDidMount() {
@@ -19,15 +27,9 @@ class App extends React.Component {
         <div className="App">
           <Header />
           <Route exact path="/" component={About} />
-          <Route exact path="/sawce" component={Sawce} />
-          {/* <Route path="/about" component={Projects} /> */}
+          <Route path="/cv" component={relocate(CV_URL)} />
+          <Route path="/setup" component={relocate(BOX_INSTALL_SCRIPT)} />
           <Route path="/logs" component={Logs} />
-          <Route
-            exact
-            strict
-            path="/:somepath"
-            render={({ match }) => <p>There is no {match.url} soz</p>}
-          />
         </div>
       </Router>
     );
